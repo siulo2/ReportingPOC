a78e237463e92531a2f904afd9993ab25b1313b1
Siu LO <siulo2@cityu.edu.hk>
2024-02-08 15:38:16 +0800

    console app create

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,62 @@
+﻿using Microsoft.Reporting.WinForms;
+using Microsoft.ReportingServices.ReportProcessing.ReportObjectModel;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.IO;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+using Oracle.ManagedDataAccess.Client;
+using Microsoft.ReportingServices.Diagnostics.Internal;
+
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static DataTable setupDataTable()
+        {
+            OracleConnection connection = new OracleConnection();
+            connection.ConnectionString = "Data Source=development_esu8.world;Persist Security Info=True;User ID=admsystem;Password=ad9835A9;";
+            connection.Open();
+
+            System.Data.DataSet ds = new System.Data.DataSet("test_dataset");
+            string sql = "SELECT * FROM test_data";
+            OracleDataAdapter da = new OracleDataAdapter(sql, connection);
+            da.Fill(ds);
+            connection.Close();
+            DataTable dt = ds.Tables[0];
+            return dt;
+        }
+
+        static void Main(string[] args)
+        {
+            SqlServerTypes.Utilities.LoadNativeAssemblies(AppDomain.CurrentDomain.BaseDirectory);
+
+            Console.WriteLine("Fetching data");
+            DataTable dataTable = setupDataTable();
+
+            Console.WriteLine("Preparing templates");
+            ReportDataSource ds = new ReportDataSource("test_dataset", dataTable);
+            LocalReport lreport = new LocalReport();
+            lreport.ReportPath = "C:/Users/siulo2/Documents/GitHub/ReportingPOC/WebApplication2/Report1.rdlc";
+            lreport.DataSources.Add(ds);
+
+            Console.WriteLine("Rendering");
+            byte[] bytes = lreport.Render("PDF");
+
+            Console.WriteLine("Wrting file");
+            DateTime d = DateTime.Now;            
+            string filename = "c:/temp/RDL-" + d.ToString("yyyyMMdd-HHmmss") + ".pdf";
+            FileStream fs = new FileStream(filename, FileMode.Create);
+            fs.Write(bytes, 0, bytes.Length);
+            fs.Close();
+
+            Console.WriteLine("Report generated to " + filename);
+        }
+    }
+}
