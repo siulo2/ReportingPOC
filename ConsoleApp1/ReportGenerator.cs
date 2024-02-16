df4fdfb3213c7a67014f7b2aec624725fbf9d6a8
Siu LO <siulo2@cityu.edu.hk>
2024-02-16 10:37:06 +0800

    showing how to use the library

---
+++ ConsoleApp1/ReportGenerator.cs
@@ -0,0 +1,68 @@
+﻿using Microsoft.Reporting.WinForms;
+using Microsoft.ReportingServices.Interfaces;
+using Microsoft.ReportingServices.ReportProcessing.ReportObjectModel;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CityU.Common.ReportEngine
+{
+    public class ReportGenerator
+    {
+        private string rdlPath;
+        private LocalReport localReport;
+
+        public ReportGenerator(string rdlPath)
+        {
+            this.rdlPath = rdlPath;
+            this.localReport = new LocalReport();
+            localReport.ReportPath = this.rdlPath;
+        }
+
+        public ReportGenerator(string rdlPath, DataTable[] dataSources) : this(rdlPath)
+        {
+            setDataSources(dataSources);
+        }
+
+        public void setDataSources(DataTable[] dataSources)
+        {
+            this.localReport.DataSources.Clear();
+            foreach (DataTable t in dataSources)
+            {
+                ReportDataSource s = new ReportDataSource(t.TableName, t);
+                this.localReport.DataSources.Add(s);
+            }
+        }
+
+        public byte[] render(string format)
+        {
+            byte[] bytes = this.localReport.Render(format);
+            return bytes;
+        }
+
+        /**
+         * Render report in a format to a temp file 
+         * @return the filename the report is written to.
+         */
+        public string renderToFile(string format)
+        {
+            return writefile(render(format));
+        }
+
+        private string writefile(byte[] data, string path = null)
+        {
+            if (path == null)
+            {
+                path = Path.GetTempFileName();
+            }
+            FileStream fs = new FileStream(path, FileMode.Create);
+            fs.Write(data, 0, data.Length);
+            fs.Close();
+            return path;
+        }
+    }
+}
--- ConsoleApp2/Program.cs
+++ ConsoleApp2/Program.cs
@@ -1,15 +1,40 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using CityU.Common.ReportEngine;
+using Oracle.ManagedDataAccess.Client;
+using System;
+using System.Configuration;
+using System.Data;
 
 namespace ConsoleApp2
 {
     internal class Program
     {
+        static DataTable setupDataTable()
+        {
+            OracleConnection connection = new OracleConnection();
+            connection.ConnectionString = ConfigurationManager.ConnectionStrings["admsystem"].ConnectionString;
+            connection.Open();
+
+            System.Data.DataSet ds = new System.Data.DataSet("test_dataset");
+            string sql = "SELECT * FROM test_data";
+            OracleDataAdapter da = new OracleDataAdapter(sql, connection);
+            da.Fill(ds);
+            connection.Close();
+            DataTable dt = ds.Tables[0];
+            return dt;
+        }
+
         static void Main(string[] args)
         {
+            DataTable dt = setupDataTable();
+            dt.TableName = "test_dataset";
+            DataTable[] datasource = new DataTable[] { dt };
+
+            string rdl = "C:/Users/siulo2/Documents/GitHub/ReportingPOC/WebApplication2/Report1.rdlc";
+
+            ReportGenerator g = new ReportGenerator(rdl, datasource);
+            string renderedfile = g.renderToFile("pdf");
+
+            Console.WriteLine("Report generated to " + renderedfile);
         }
     }
 }
