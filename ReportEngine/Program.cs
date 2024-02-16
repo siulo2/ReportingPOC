2c5e1805c736f6264d24833ac6b56001f93c6563
Siu LO <siulo2@cityu.edu.hk>
2024-02-16 11:51:57 +0800

    re-arrange project folders

---
+++ ReportEngine/Program.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.Reporting.WinForms;
+using Oracle.ManagedDataAccess.Client;
+using System;
+using System.Configuration;
+using System.Data;
+using System.IO;
+
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            
+        }
+    }
+}
